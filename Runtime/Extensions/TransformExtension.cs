6cde2199fcdd0e8134fd4094a8539481dd49f477
DiscreteTom <discrete_tom@outlook.com>
2023-04-28 15:32:43 +0800

    feat: TransformExtension return self

--- Runtime/Extensions/TransformExtension.cs
+++ Runtime/Extensions/TransformExtension.cs
@@ -3,65 +3,83 @@ using UnityEngine;
 
 namespace DT.UniUtils {
   public static class TransformExtension {
-    public static void SetPositionX(this Transform t, float value) {
+    public static Transform SetPositionX(this Transform t, float value) {
       t.position = t.position.WithX(value);
+      return t;
     }
-    public static void SetPositionY(this Transform t, float value) {
+    public static Transform SetPositionY(this Transform t, float value) {
       t.position = t.position.WithY(value);
+      return t;
     }
-    public static void SetPositionZ(this Transform t, float value) {
+    public static Transform SetPositionZ(this Transform t, float value) {
       t.position = t.position.WithZ(value);
+      return t;
     }
-    public static void SetLocalPositionX(this Transform t, float value) {
+    public static Transform SetLocalPositionX(this Transform t, float value) {
       t.localPosition = t.localPosition.WithX(value);
+      return t;
     }
-    public static void SetLocalPositionY(this Transform t, float value) {
+    public static Transform SetLocalPositionY(this Transform t, float value) {
       t.localPosition = t.localPosition.WithY(value);
+      return t;
     }
-    public static void SetLocalPositionZ(this Transform t, float value) {
+    public static Transform SetLocalPositionZ(this Transform t, float value) {
       t.localPosition = t.localPosition.WithZ(value);
+      return t;
     }
-    public static void SetLocalScaleX(this Transform t, float value) {
+    public static Transform SetLocalScaleX(this Transform t, float value) {
       t.localScale = t.localScale.WithX(value);
+      return t;
     }
-    public static void SetLocalScaleY(this Transform t, float value) {
+    public static Transform SetLocalScaleY(this Transform t, float value) {
       t.localScale = t.localScale.WithY(value);
+      return t;
     }
-    public static void SetLocalScaleZ(this Transform t, float value) {
+    public static Transform SetLocalScaleZ(this Transform t, float value) {
       t.localScale = t.localScale.WithZ(value);
+      return t;
     }
-    public static void SetRotationX(this Transform t, float value) {
+    public static Transform SetRotationX(this Transform t, float value) {
       t.rotation = t.rotation.WithX(value);
+      return t;
     }
-    public static void SetRotationY(this Transform t, float value) {
+    public static Transform SetRotationY(this Transform t, float value) {
       t.rotation = t.rotation.WithY(value);
+      return t;
     }
-    public static void SetRotationZ(this Transform t, float value) {
+    public static Transform SetRotationZ(this Transform t, float value) {
       t.rotation = t.rotation.WithZ(value);
+      return t;
     }
-    public static void SetRotationW(this Transform t, float value) {
+    public static Transform SetRotationW(this Transform t, float value) {
       t.rotation = t.rotation.WithW(value);
+      return t;
     }
-    public static void SetLocalRotationX(this Transform t, float value) {
+    public static Transform SetLocalRotationX(this Transform t, float value) {
       t.localRotation = t.localRotation.WithX(value);
+      return t;
     }
-    public static void SetLocalRotationY(this Transform t, float value) {
+    public static Transform SetLocalRotationY(this Transform t, float value) {
       t.localRotation = t.localRotation.WithY(value);
+      return t;
     }
-    public static void SetLocalRotationZ(this Transform t, float value) {
+    public static Transform SetLocalRotationZ(this Transform t, float value) {
       t.localRotation = t.localRotation.WithZ(value);
+      return t;
     }
-    public static void SetLocalRotationW(this Transform t, float value) {
+    public static Transform SetLocalRotationW(this Transform t, float value) {
       t.localRotation = t.localRotation.WithW(value);
+      return t;
     }
 
-    public static void ForEachChild(this Transform t, Action<Transform, int> action) {
+    public static Transform ForEachChild(this Transform t, Action<Transform, int> action) {
       for (int i = 0; i < t.childCount; i++) {
         action(t.GetChild(i), i);
       }
+      return t;
     }
-    public static void ForEachChild(this Transform t, Action<Transform> action) {
-      t.ForEachChild((t, i) => action(t));
+    public static Transform ForEachChild(this Transform t, Action<Transform> action) {
+      return t.ForEachChild((t, i) => action(t));
     }
     public static T[] MapChildren<T>(this Transform t, Func<Transform, int, T> action) {
       T[] result = new T[t.childCount];
