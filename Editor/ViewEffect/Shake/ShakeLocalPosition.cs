957d20bb812886b12fc796686ed5a27a71a271f7
DiscreteTom <discrete_tom@outlook.com>
2023-04-28 20:29:41 +0800

    feat!: remove Shake.shaking/shake

--- Editor/ViewEffect/Shake/ShakeLocalPosition.cs
+++ Editor/ViewEffect/Shake/ShakeLocalPosition.cs
@@ -2,37 +2,23 @@ using UnityEngine;
 
 namespace DT.UniUtils.View {
   public class ShakeLocalPosition : ShakeBehaviour {
-    [SerializeField] bool shakeZ = false;
-    public bool shake {
-      get {
-        return this.shaking;
-      }
-      set {
-        if (value) {
-          this.shaking = true;
-          this.initPos = this.transform.localPosition;
-        } else {
-          this.shaking = false;
-          this.transform.localPosition = this.initPos;
-        }
-      }
-    }
+    public bool shakeZ = false;
 
     Vector3 initPos;
 
     void Start() {
-      if (this.shakeAtStart) {
-        this.shake = true;
-      }
+      this.initPos = this.transform.localPosition;
     }
 
     void Update() {
-      if (!this.shaking) return;
-
       var x = this.GetShakeValue(this.initPos.x);
       var y = this.GetShakeValue(this.initPos.y);
       var z = this.shakeZ ? this.GetShakeValue(this.initPos.z) : this.initPos.z;
       this.transform.localPosition = new Vector3(x, y, z);
+    }
+
+    void OnDisable() {
+      this.transform.localPosition = this.initPos;
     }
   }
 }
