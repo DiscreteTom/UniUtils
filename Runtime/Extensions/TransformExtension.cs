f05223c29a023ea639d38b472dd6e4938452285a
DiscreteTom <discrete_tom@outlook.com>
2023-04-28 14:57:43 +0800

    chore: optimize code

--- Runtime/Extensions/TransformExtension.cs
+++ Runtime/Extensions/TransformExtension.cs
@@ -3,33 +3,33 @@ using UnityEngine;
 
 namespace DT.UniUtils {
   public static class TransformExtension {
-    public static void SetPositionX(this Transform original, float value) {
-      original.position = original.position.WithX(value);
+    public static void SetPositionX(this Transform t, float value) {
+      t.position = t.position.WithX(value);
     }
-    public static void SetPositionY(this Transform original, float value) {
-      original.position = original.position.WithY(value);
+    public static void SetPositionY(this Transform t, float value) {
+      t.position = t.position.WithY(value);
     }
-    public static void SetPositionZ(this Transform original, float value) {
-      original.position = original.position.WithZ(value);
+    public static void SetPositionZ(this Transform t, float value) {
+      t.position = t.position.WithZ(value);
     }
 
-    public static void ForEachChild(this Transform original, Action<Transform, int> action) {
-      for (int i = 0; i < original.childCount; i++) {
-        action(original.GetChild(i), i);
+    public static void ForEachChild(this Transform t, Action<Transform, int> action) {
+      for (int i = 0; i < t.childCount; i++) {
+        action(t.GetChild(i), i);
       }
     }
-    public static void ForEachChild(this Transform original, Action<Transform> action) {
-      original.ForEachChild((t, i) => action(t));
+    public static void ForEachChild(this Transform t, Action<Transform> action) {
+      t.ForEachChild((t, i) => action(t));
     }
-    public static T[] MapChildren<T>(this Transform original, Func<Transform, int, T> action) {
-      T[] result = new T[original.childCount];
-      for (int i = 0; i < original.childCount; i++) {
-        result[i] = action(original.GetChild(i), i);
+    public static T[] MapChildren<T>(this Transform t, Func<Transform, int, T> action) {
+      T[] result = new T[t.childCount];
+      for (int i = 0; i < t.childCount; i++) {
+        result[i] = action(t.GetChild(i), i);
       }
       return result;
     }
-    public static T[] MapChildren<T>(this Transform original, Func<Transform, T> action) {
-      return original.MapChildren((t, i) => action(t));
+    public static T[] MapChildren<T>(this Transform t, Func<Transform, T> action) {
+      return t.MapChildren((t, i) => action(t));
     }
   }
 }
